453d9ade282a4e9b385e75cfcc39144029d538d8
Etienne <cellier.etienne@gmail.com>
2023-04-06 09:44:45 +0200

    Best Youtube pt.3

--- app/src/main/java/com/webtech/mybestyoutube/MainActivity.java
+++ app/src/main/java/com/webtech/mybestyoutube/MainActivity.java
@@ -1,14 +1,37 @@
 package com.webtech.mybestyoutube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivity context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                Intent intent = new Intent(context, AddActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
